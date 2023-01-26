999d3df86ab8c84e7e416a9695dc09fc828dea23
KieMerr17 <kieran.merrett@hotmail.com>
2023-01-26 05:38:31 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,32 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`)
+                runGame(gameType)
             }
         })
     }
+
+    runGame("addition");
+
 })
 
-function runGame() {
+/**
+ * The main game "loop called " when script.js first laoded
+ * and after the users answer has been processed
+ */
+
+function runGame(gameType) {
+
+    // creates a random number between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion (num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType} aborting`
+    }
 }
 
 function checkAnswer() {
@@ -37,7 +54,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
