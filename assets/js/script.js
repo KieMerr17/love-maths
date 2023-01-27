a510a32fdd9ee1a3811c49d4a18d52311aa7b65c
KieMerr17 <kieran.merrett@hotmail.com>
2023-01-27 01:16:18 +0000

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion (num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion (num1 , num2);
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType} aborting`;
@@ -69,6 +71,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"]
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"]
     } else {
         alert(`Unimplimented operator ${operator}`)
         throw `Unimplimented operator ${operator}. Aborting!`;
@@ -97,6 +101,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
